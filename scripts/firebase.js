546e7798a1823bb9711c332a45d416094000ad83
MediSync Admin <user@medisync.ai>
2026-04-24 02:08:14 +0530

    chore: updated firebase config and admin UID

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -5,14 +5,13 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB5PsgVUGeHAyXjzuU7Yqw9QhN8wY0ssIA",
-    authDomain: "aarogya-281e3.firebaseapp.com",
-    databaseURL: "https://aarogya-281e3-default-rtdb.firebaseio.com",
-    projectId: "aarogya-281e3",
-    storageBucket: "aarogya-281e3.firebasestorage.app",
-    messagingSenderId: "676098023928",
-    appId: "1:676098023928:web:dbd4fd613973dee526b60d",
-    measurementId: "G-X5CQZMRXMJ"
+    apiKey: "AIzaSyBc_6EVKN3h3PojI8Fv7aHyyKCqnp08vdY",
+    authDomain: "aaro-7786a.firebaseapp.com",
+    databaseURL: "https://aaro-7786a-default-rtdb.firebaseio.com",
+    projectId: "aaro-7786a",
+    storageBucket: "aaro-7786a.firebasestorage.app",
+    messagingSenderId: "641143458909",
+    appId: "1:641143458909:web:2d05fc159480fd1ed591b4"
 };
 
 const app = initializeApp(firebaseConfig);
